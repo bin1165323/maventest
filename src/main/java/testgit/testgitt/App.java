37ae80d81247b0b58d711ec9cbc1707b6ab120fe
bin1165323 <503775882@qq.com>
2019-10-19 08:21:32 +0800

    test

---
+++ src/main/java/testgit/testgitt/App.java
@@ -0,0 +1,13 @@
+package testgit.testgitt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
